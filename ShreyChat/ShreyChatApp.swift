b07d30dadf0ad7f3639a8c8e867fbd058c8b4ebd
Shreyas Sahoo <shreyassahoo26@gmail.com>
2026-02-09 21:58:43 +0530

    Initial Commit

---
+++ ShreyChat/ShreyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShreyChatApp.swift
+//  ShreyChat
+//
+//  Created by Shreyas on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ShreyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShreyChatTests/ShreyChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShreyChatTests.swift
+//  ShreyChatTests
+//
+//  Created by Shreyas on 09/02/26.
+//
+
+import Testing
+@testable import ShreyChat
+
+struct ShreyChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
